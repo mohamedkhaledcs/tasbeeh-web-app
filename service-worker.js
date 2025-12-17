28c540ae60b7869c834efb15cf566180f6e5b002
mohamed khaled <mohamed.khaled.career@gmail.com>
2025-12-17 06:55:50 +0200

    Update service worker cache version

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tasbeeh-app-v2";
+const CACHE_NAME = "tasbeeh-app-v3";
 
 const ASSETS = [
   "./",
@@ -8,11 +8,26 @@ const ASSETS = [
   "./manifest.json"
 ];
 
-
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
@@ -20,4 +35,5 @@ self.addEventListener("fetch", event => {
     caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
+
 
